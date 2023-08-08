2595f80b763164df84bd0d00d405f5bf5f26e451
Matthew Mueller <mattmuelle@gmail.com>
2023-08-08 01:48:25 -0500

    remove unused code

--- internal/token/token.go
+++ internal/token/token.go
@@ -37,31 +37,3 @@ const (
 	Star       Type = "*"
 	Pipe       Type = "|"
 )
-
-// func Expand(tokens []Token) (tokensets [][]Token) {
-// 	for i, token := range tokens {
-// 		switch token.Type {
-// 		case Question:
-// 			tokensets = append(tokensets, stripTokenTrail(tokens[:i]))
-// 			tokens[i] = Token{Type: Slot, Text: token.Text, Start: token.Start, Line: token.Line}
-// 		case Star:
-// 			tokensets = append(tokensets, stripTokenTrail(tokens[:i]))
-// 			tokens[i] = Token{Type: Slot, Text: token.Text, Start: token.Start, Line: token.Line}
-// 		}
-// 	}
-// 	tokensets = append(tokensets, tokens)
-// 	return tokensets
-// }
-
-// // strip token trail removes path tokens up to either a slot or a slash
-// // e.g. /:id. => /:id
-// //
-// //	/a/b => /a
-// func stripTokenTrail(tokens []Token) []Token {
-// 	for i := len(tokens) - 1; i >= 0; i-- {
-// 		if tokens[i].Type != Slash {
-// 			return tokens[0:i]
-// 		}
-// 	}
-// 	return tokens
-// }
